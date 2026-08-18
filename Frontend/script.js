347d36fc77604bc2d54c4f06f03b10ff02757fb4
Rachin02 <rachin15-5466@diu.edu.bd>
2026-08-18 16:37:33 +0600

    backend hosted on aws

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,7 +2,7 @@
 // CONFIGURATION
 // ============================================================
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://closure-glen-innovation-summit.trycloudflare.com";
 
 
 // ============================================================
